038ef92ad3a6782be73851a012937c136d5c51a0
Tyler <tsumararaiko@gmail.com>
2023-02-03 23:32:59 -0500

    added array of stories to data initializations

---
+++ static/js/data_initializations.js
@@ -0,0 +1,17 @@
+let stories = [
+    new Event (
+        "The Witch's Dilemna",
+        "witch.jpg",
+        "Donec libero mauris, hendrerit eu turpis venenatis, vestibulum volutpat purus. Ut pharetra convallis nulla vitae venenatis. Mauris gravida turpis dui. Nullam posuere diam id arcu varius egestas. Phasellus non laoreet arcu, vitae mattis nunc. In non lorem arcu. Praesent semper tempor tristique. Integer commodo faucibus nunc et iaculis. Cras ac venenatis libero, nec hendrerit arcu. Sed pretium justo mauris, id accumsan sapien eleifend vel. Quisque quis elementum quam.",
+        "Attempt to help",
+        "Leave for now...",
+        "You did the thing",
+        "You decided to leave",
+        () => {
+            console.log('Choice 1!')
+        },
+        () => {
+            console.log('Choice 2!')
+        }
+    )
+]
