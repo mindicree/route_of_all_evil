35e1df462f39300158b1d8d5856b820c95157d8c
Tyler <tsumararaiko@gmail.com>
2023-02-04 11:43:23 -0500

    added update ui functions

--- static/js/game_functions.js
+++ static/js/game_functions.js
@@ -7,6 +7,19 @@ function show_screen(old_screen, new_screen) {
     old_screen.classList.remove('z-50')
 }
 
+function update_ui() {
+    update_intro()
+    update_route()
+    update_merchant()
+    update_story()
+    update_storyresult()
+    update_combat()
+    update_combatresult()
+    update_boss()
+    update_orb()
+    update_newitem()
+}
+
 // initial views
 setTimeout(() => {
     game_screen_container.classList.remove('opacity-0')
---
+++ static/js/update_ui_functions.js
@@ -0,0 +1,30 @@
+function update_intro() {
+    
+}
+function update_route(new_routes=False) {
+
+}
+function update_merchant() {
+    
+}
+function update_story() {
+    
+}
+function update_storyresult() {
+    
+}
+function update_combat() {
+    
+}
+function update_combatresult() {
+    
+}
+function update_boss() {
+    
+}
+function update_orb() {
+    
+}
+function update_newitem() {
+    
+}
