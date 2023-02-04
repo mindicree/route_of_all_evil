45772354f8fe70e75de037a3f7e51456d38324ea
Tyler <tsumararaiko@gmail.com>
2023-02-04 11:25:17 -0500

    added game over screen

--- static/js/game_initializations.js
+++ static/js/game_initializations.js
@@ -1,12 +1,14 @@
-let player;
+let save_data = localStorage.getItem('save_data');
 
-let current_enemy;
-let current_boss;
-let current_routes;
-let current_story;
-let current_level;
-
-let current_screen;
+let player = (save_data ? save_data['player'] : new Player("Gideon", 20, 10, 10));
+let player_history = (save_data ? save_data['player_history'] : []);
+let current_enemy = (save_data ? save_data['current_enemy'] : null);
+let current_boss = (save_data ? save_data['current_boss'] : null);
+let current_routes = (save_data ? save_data['current_routes'] : []);
+let current_story = (save_data ? save_data['current_story'] : null);
+let current_level = (save_data ? save_data['current_level'] : 1);
+let current_screen = (save_data ? save_data['current_screen'] : null);
+let bosses_defeated = (save_data ? save_data['bosses_defeated'] : 0);
 
 // screen initializations
 let game_screen_container = document.querySelector('#game_screen_container')
@@ -23,3 +25,4 @@ let screen_newitem = document.querySelector('#screen_newitem')
 let screen_tutorial = document.querySelector('#screen_tutorial')
 let screen_credits = document.querySelector('#screen_credits')
 let screen_intro = document.querySelector('#screen_intro')
+let screen_gameover = document.querySelector('#screen_gameover')
