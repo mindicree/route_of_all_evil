8691e31aa001d5fa4d1f0245af18cbca80e19c4f
Tyler <tsumararaiko@gmail.com>
2023-02-04 01:17:26 -0500

    added fonts and ripple, finished loading screen

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
+@app.route('/fonts/<font_name>')
+def get_font(font_name):
+    return send_file(f'./fonts/{font_name}')
 
 if __name__ == '__main__':
     app.run(debug=True)
