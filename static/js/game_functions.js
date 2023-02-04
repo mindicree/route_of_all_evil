740ec7c05c742e422af686d2d60f2245338604dd
Tyler <tsumararaiko@gmail.com>
2023-02-04 11:47:53 -0500

    renamed show_screen to transition_screen

--- static/js/game_functions.js
+++ static/js/game_functions.js
@@ -1,4 +1,4 @@
-function show_screen(old_screen, new_screen) {
+function transition_screen(old_screen, new_screen) {
     old_screen.classList.add('inactive_screen')
     new_screen.classList.remove('inactive_screen')
     new_screen.classList.add('z-50')
@@ -23,5 +23,5 @@ function update_ui() {
 // initial views
 setTimeout(() => {
     game_screen_container.classList.remove('opacity-0')
-    show_screen(screen_loading, screen_home)
+    transition_screen(screen_loading, screen_home)
 }, 2000);
