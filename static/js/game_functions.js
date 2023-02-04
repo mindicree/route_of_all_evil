e70fc0a815a89de521e1f3103bc2c651b2ee8230
Tyler <tsumararaiko@gmail.com>
2023-02-04 03:03:08 -0500

    got home screen hovering to work
    issue was due to z-index issues w other screens

--- static/js/game_functions.js
+++ static/js/game_functions.js
@@ -1,8 +1,10 @@
 function show_screen(old_screen, new_screen) {
     old_screen.classList.add('inactive_screen')
     new_screen.classList.remove('inactive_screen')
+    new_screen.classList.add('z-50')
     new_screen.classList.add('active_screen')
     old_screen.classList.remove('active_screen')
+    old_screen.classList.remove('z-50')
 }
 
 // initial views
