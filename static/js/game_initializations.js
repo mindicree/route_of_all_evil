8a12a79b295be3248d79b287104cb2caffdb0f0f
Tyler <tsumararaiko@gmail.com>
2023-02-05 03:11:01 -0500

    removed stupid inits from game init

--- static/js/game_initializations.js
+++ static/js/game_initializations.js
@@ -2,13 +2,13 @@ let save_data = localStorage.getItem('save_data');
 
 let player = (save_data ? save_data['player'] : new Player("Gideon", 20, 10, 10));
 let player_history = (save_data ? save_data['player_history'] : []);
-let current_enemy = (save_data ? save_data['current_enemy'] : null);
-let current_boss = (save_data ? save_data['current_boss'] : null);
-let current_routes = (save_data ? save_data['current_routes'] : []);
-let current_story = (save_data ? save_data['current_story'] : null);
-let current_level = (save_data ? save_data['current_level'] : 1);
-let current_screen = (save_data ? save_data['current_screen'] : null);
-let bosses_defeated = (save_data ? save_data['bosses_defeated'] : []);
+let current_enemy = null
+let current_boss = null
+let current_routes = []
+let current_story = null
+let current_level = 1
+let current_screen = null
+let bosses_defeated = []
 
 // screen initializations
 let game_screen_container = document.querySelector('#game_screen_container')
