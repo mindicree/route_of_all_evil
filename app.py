750c237888ad54239b08f582c5e7cbda290cdfa5
Tyler Johnson <tsumararaiko@gmail.com>
2023-02-07 21:05:46 -0500

    added dedicated config file

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, send_file
 from random import random
+import json
 
 app = Flask(__name__)
+
+try:
+    with open('config.yaml', 'r') as file:
+        config = json.load(file)
+except Exception as e:
+    print('Could not load config file. Using Default values')
+    config = {
+        'HOST': '127.0.0.1',
+        'PORT': '5000',
+        'DEBUG': True
+    }
 
 names_list = []
 with open('static/names.txt', 'r') as file:
@@ -27,4 +39,4 @@ def get_image(cat, image_name):
     return send_file(f'./static/img/{cat}/{image_name}')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host=config['HOST'], port=config['PORT'], debug=config['DEBUG'])
